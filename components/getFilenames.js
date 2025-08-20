30f666688a21a31486dd16cd55032fb6e8ae4138
Bryce <brycecoulson@gmail.com>
2025-08-19 21:57:04 -0700

    adding debug to getFilenames 	modified:   components/getFilenames.js

--- components/getFilenames.js
+++ components/getFilenames.js
@@ -1,7 +1,14 @@
 export async function getFilenames(folder) {
+    console.log(`Fetching filenames from ${folder}`);
     const response = await fetch(folder);
+    console.log(`Response status: ${response.status}`);
     const data = await response.text();
-    const filenames = data.match(/title="([^"]+)"/g).map(f => f.replace(/title="|"/g, ''));
-    const filteredFilenames = filenames.filter(filename => filename !== '..').map(filename => filename.replace('.js', '').replace(/^_/, ''));
+    console.log(`Response data: ${data}`);
+    const filenames = data.match(/title="([^"]+)"/g);
+    console.log(`Filenames found: ${filenames}`);
+    const filteredFilenames = filenames.map(f => f.replace(/title="|"/g, '')).filter(filename => filename !== '..').map(filename => filename.replace('.js', '').replace(/^_/, ''));
+    console.log(`Filtered filenames: ${filteredFilenames}`);
     return filteredFilenames;
+
+
 }
